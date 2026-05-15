55d659718851e79d8e2b5141b21bf93b0533003f
Azi Syahrijal <azisyahrijal8@gmail.com>
2026-05-15 16:21:11 +0700

    Add files via upload

---
+++ storage/framework/views/a3575181e3a18df95a5da34155f572a6.php
@@ -0,0 +1,35 @@
+
+<?php $__env->startSection('content'); ?>
+
+<div class="d-flex justify-content-center">
+  <div class="card mb-3" style="max-width: 540px; width:100%;">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex align-items-center justify-content-center p-3">
+        <img src="<?php echo e(asset('storage/' . ($row->foto ?? 'profile.jpg'))); ?>"
+             class="img-fluid rounded img-thumbnail"
+             alt="..."
+             width="180">
+      </div>
+
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title fw-bold border-bottom pb-2 mb-3">Detail Staff</h5>
+          <p class="card-text">Nama: <?php echo e($row->name); ?></p>
+          <p class="card-text">NIP: <?php echo e($row->nip); ?></p>
+          <p class="card-text">Jenis Kelamin: <?php echo e($row->gender); ?></p>
+          <p class="card-text">Email: <?php echo e($row->email); ?></p>
+          <p class="card-text">Alamat: <?php echo e($row->alamat); ?></p>
+
+          <hr/>
+
+          <a href="<?php echo e(url('/staff')); ?>" class="btn btn-primary">
+            Back
+          </a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.index', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-app\resources\views/staff/show.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d180221a7d39dd55e28337ab286f733a.php
@@ -0,0 +1,134 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="card">
+    <div class="card-body">
+        <br />
+        <?php if($errors->any()): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><?php echo e($error); ?></li>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+        <h5 class="card-title">Form Staff</h5>
+        <!-- No Labels Form -->
+        <form class="row g-3" method="POST" action="<?php echo e(route('staff.store')); ?>"
+            enctype="multipart/form-data">
+            <?php echo csrf_field(); ?>
+            <div class="col-md-12">
+                <label for="basic-url" class="form-label">NIP</label>
+                <input type="text" class="form-control" name="nip"
+                    value="" placeholder="NIP">
+                <?php $__errorArgs = ['nip'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="invalid-feedback"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+            <div class="col-md-12">
+                <label for="basic-url" class="form-label">Nama</label>
+                <input type="text" class="form-control" name="name"
+                    value="" placeholder="Nama Staff">
+                <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="invalid-feedback"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+            
+            <div class="col-md-12">
+                <fieldset class="row mb-3r">
+                    <label for="basic-url" class="form-label">Jenis Kelamin</label>
+                    <div class="col-sm-10">
+                        <?php $__currentLoopData = $ar_gender; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $g): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" value=" <?php echo e($g); ?>" >
+                                <label class="form-check-label">
+                                    <?php echo e($g); ?>
+
+                                </label>
+                            </div>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </div>
+                </fieldset>
+                <?php $__errorArgs = ['gender'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <font color="red"><?php echo e($message); ?></font>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?> 
+            </div>
+            
+            <div class="col-12">
+                <label for="basic-url" class="form-label">Alamat</label>
+                <textarea class="form-control" 
+                 name="alamat" cols="50" rows="5"></textarea>
+                 <?php $__errorArgs = ['alamat'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="invalid-feedback"><?php echo e($message); ?></div>
+                 <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>  
+            </div>
+            <div class="col-md-12">
+                <input type="text" class="form-control" 
+                 name="email" value="" placeholder="Email">
+                 <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="invalid-feedback"><?php echo e($message); ?></div>
+                 <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>  
+            </div>
+            <div class="col-md-12">
+                <label for="basic-url" class="form-label">Foto</label>
+                
+                <input type="file" class="form-control" name="foto">
+
+                <?php $__errorArgs = ['foto'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="invalid-feedback d-block">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                <a href="<?php echo e(url('/staff')); ?>" class="btn btn-secondary btn-sm">Batal</a>
+            </div>
+        </form>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.index', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-app\resources\views/staff/form.blade.php ENDPATH**/ ?>
